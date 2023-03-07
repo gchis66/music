04fa80100d2dbb1da5573ceb0bfb559b5a714052
gchis66 <gchis66@gmail.com>
2023-03-07 00:24:24 -0500

    Internationalization
    Added the i18n package and added lite translations for french

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,6 +37,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">{{
+              currentLocale
+            }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -51,6 +58,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "fr" ? "French" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -63,6 +73,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "fr" ? "en" : "fr";
     },
   },
 };
