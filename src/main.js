26ca47f074cfd489f280db14f50bed20ac0dccd9
gchis66 <gchis66@gmail.com>
2023-03-09 15:44:11 -0500

    rollback
    rollback push to new main branch

--- src/main.js
+++ src/main.js
@@ -6,15 +6,9 @@ import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
-<<<<<<< HEAD
 import i18n from "./includes/i18n";
-import { registerSW } from "virtual:pwa-register";
-=======
->>>>>>> parent of 04fa801 (Internationalization)
 import "./assets/base.css";
 import "./assets/main.css";
-
-registerSW({ immediate: true });
 
 let app;
 auth.onAuthStateChanged(() => {
