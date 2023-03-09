9790c9422e6c767c6c8e42a4a30a21f27e9c3b2a
gchis66 <gchis66@gmail.com>
2023-03-09 11:28:45 -0500

    PWA attributes added
    Added PWA functionality and manifest file

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -63,6 +63,17 @@ export default {
         if (file.type !== "audio/mpeg") {
           return;
         }
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: "bg-red-400",
+            icon: "fas-fa-times",
+            text_class: "text-red-400",
+          });
+          return;
+        }
         const storageRef = storage.ref(); // music-da0a0.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`);
         const task = songsRef.put(file);
--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -16,6 +16,9 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+db.enablePersistence().catch((error) => {
+  console.log(`Firebase persistance error ${error.code}`);
+});
 const usersCollection = db.collection("users");
 const songsCollection = db.collection("songs");
 const commentsCollection = db.collection("comments");
